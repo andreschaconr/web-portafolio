47a9c0d037187a48f7931b99a0f0fde71887000b
andreschaconr <andreschacon635@gmail.com>
2021-12-16 21:38:27 -0500

    add section about me

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
+  menuToggle.classList.toggle('active'); //show menu
   showcase.classList.toggle('active');
 })
 
