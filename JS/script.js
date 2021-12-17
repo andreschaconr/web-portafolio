4f1bcca19d628b8e0199b0a2c9db735926f21f92
andreschaconr <andreschacon635@gmail.com>
2021-12-17 00:50:27 -0500

    add new section

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,10 @@ new Typed('#typed',{ //typed in page home
   loop : true 
  
 });
+
+
+AOS.init({
+  offset: 400, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 2000 // values from 0 to 3000, with step 50ms
+});
